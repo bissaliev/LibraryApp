cb98837d66c32cfe5b3372cf1aa8502204e929c0
bissaliev <bissaliev21@gmail.com>
2024-11-21 12:05:43 +0400

    Добавил сырой поиск по полям title, id, author

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
-# • id (уникальный идентификатор, генерируется автоматически)
-#  • title (название книги)
-#  • author (автор книги)
-#  • year (год издания)
-#  • status (статус книги: “в наличии”, “выдана”)
-
-import dataclasses
 import json
 
 
-@dataclasses
-class Book2:
-    title: str
-    author: str
-    year: int
-    status: str = "в наличии"
-    book_id: int | None = None
-
-
 class Book:
-    def __init__(self, title, author, year, id=None, status="в наличии"):
+    def __init__(
+        self,
+        title: str,
+        author: str,
+        year: int,
+        id: int = None,
+        status: str = "в наличии",
+    ):
         self.id: int = id
         self.title: str = title
         self.author: str = author
@@ -26,10 +17,10 @@ class Book:
         self.status: str = status
 
     def __str__(self):
-        return (
-            f"id: {self.id}, title: {self.title}, author: {self.author}, "
-            f"year: {self.year}, status: {self.status}"
-        )
+        return str(self.to_dict())
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.title})"
 
     def to_dict(self):
         """Преобразование объекта книги в словарь."""
@@ -46,32 +37,48 @@ class Book:
         """Создание объекта книги из словаря."""
         return cls(**data)
 
+    def __eq__(self, other: "Book"):
+        return (self.title, self.author, self.year) == (
+            other.title,
+            other.author,
+            other.year,
+        )
+
 
 class LibraryManager:
     def __init__(self, filename: str = "library.json"):
-        self.filename = filename
-        self.books = {}
+        self.filename: str = filename
+        self.books: dict[str, Book] = {}
         self.load_book()
 
     def load_book(self):
-        with open(self.filename, encoding="utf-8") as file:
-            data = json.load(file)
-            for book_data in data:
-                book = Book(**book_data)
-                self.books[book.id] = book
+        try:
+            with open(self.filename, encoding="utf-8") as file:
+                data = json.load(file)
+                self.books = {
+                    key: Book(**value) for key, value in data.items()
+                }
+        except FileNotFoundError:
+            print(f"Файла `{self.filename}` не существует.")
 
     def save_books(self):
         with open(self.filename, "w", encoding="utf-8") as file:
-            data = [book.to_dict() for book in self.books.values()]
+            data = {book.title: book.to_dict() for book in self.books.values()}
             json.dump(data, file, ensure_ascii=False, indent=4)
 
+    def get_book(self, id):
+        return self.books.get(str(id))
+
     def get_books(self):
-        for book in self.books.values():
-            print(book)
+        yield from self.books.values()
 
     def add_book(self, title: str, author: str, year: int):
-        book_id = max(self.books.keys()) + 1
-        book = Book(title=title, author=author, year=year, id=book_id)
+        book = Book(title=title, author=author, year=year)
+        if book.title in self.books and book == self.books[book.title]:
+            print(f"Книга `{book.title}` уже существует.")
+            return
+        next_id = max(book.id for book in self.books.values()) + 1
+        book.id = next_id
         self.books[book.id] = book
         self.save_books()
         return book
@@ -100,24 +107,41 @@ class LibraryManager:
 
     def search_book(self, field: str, query: str):
         """Поиск книг по полям title, author, year."""
+        field = field.lower()
+        return getattr(self, f"_get_book_by_{field}")(query)
+
+    def _binary_search(objs: list[Book], id):
+        left = 0
+        right = len(objs)
+        while left < right:
+            middle = (left + right) // 2
+            obj = objs[middle]
+            if id > obj.id:
+                left = middle + 1
+            elif id < obj.id:
+                right = middle
+            else:
+                return obj
+        return objs[left] if objs[left].id == id else []
+
+    def _get_book_by_id(self, id: int):
+        return self._binary_search(list(self.books.values()), id)
+
+    def _get_book_by_author(self, author: str):
+        output = []
         for book in self.books.values():
-            if (
-                getattr(book, field.lower()) == query.title()
-                if isinstance(query, str)
-                else query
-            ):
-                print("Книга найдена:")
-                print(book)
-                return
-        print(f"Книга со значением `{query}` не найдена.")
+            if book.author == author:
+                output.append(book)
+        return output
+
+    def _get_book_by_title(self, title: str):
+        return [self.books.get(title)]
 
 
-library = LibraryManager()
-library.get_books()
-# library.add_book(
-#     title="Над пропастью во ржи2222", author="Лев Толстой", year=1869
-# )
-# library.delete_book(7)
-# library.update_book_status(6, "выдана")
-# library.get_books()
-library.search_book("year", 1949)
+if __name__ == "__main__":
+    library = LibraryManager()
+    for book in library.get_books():
+        print(book)
+    print(library.search_book("author", "Джоан Роулинг"))
+    library.add_book("Гарри Поттер и Дары Смерти", "Джоан Роулинг", 2007)
+    print(library.search_book("author", "Джоан Роулинг"))
