b324872573446af36846b7b1bdb55c0eb68918f1
bissaliev <bissaliev21@gmail.com>
2024-11-24 17:28:13 +0400

    Рефакторинг модуля books.py

--- books.py
+++ books.py
@@ -3,11 +3,15 @@ from enum import Enum
 
 
 class Status(Enum):
+    """Статусы книг."""
+
     AVAILABLE: str = "в наличии"
     BORROWED: str = "выдана"
 
 
 class Book:
+    """Класс книг."""
+
     def __init__(
         self,
         title: str,
@@ -46,16 +50,19 @@ class Book:
 
     @classmethod
     def from_dict(cls, data: dict[str : str | int]) -> "Book":
-        """Создание объекта из словаря."""
+        """Классовый метод для создания объекта из словаря."""
         return cls(
             id=data.get("id"),
             title=data.get("title"),
             author=data.get("author"),
             year=data.get("year"),
-            status=data.get("status"),
+            status=data.get("status", Status.AVAILABLE.value),
         )
 
-    def __eq__(self, other: "Book") -> bool:
+    def __eq__(self, other: object) -> bool:
+        """Метод сравнения объектов по полям title, author, year."""
+        if not isinstance(other, Book):
+            return NotImplemented
         return (
             self.title.lower() == other.title.lower()
             and self.author.lower() == other.author.lower()
@@ -64,10 +71,12 @@ class Book:
 
     @property
     def id(self) -> int:
+        """Геттер для атрибута id."""
         return self.__id
 
     @id.setter
     def id(self, value: int) -> None:
+        """Сеттер для атрибута id."""
         if not isinstance(value, int) or value < 1:
             raise ValueError(
                 "ID книги должно быть целым положительным числом."
@@ -76,26 +85,32 @@ class Book:
 
     @property
     def title(self) -> str:
+        """Геттер для атрибута title."""
         return self.__title
 
     @title.setter
     def title(self, value: str) -> None:
+        """Сеттер для атрибута title."""
         self.__title = self.validate_non_empty_string("title", value)
 
     @property
     def author(self) -> str:
+        """Геттер для атрибута author."""
         return self.__author
 
     @author.setter
     def author(self, value: str) -> None:
+        """Сеттер для атрибута author."""
         self.__author = self.validate_non_empty_string("author", value)
 
     @property
     def year(self) -> int:
+        """Геттер для атрибута year."""
         return self.__year
 
     @year.setter
     def year(self, value: int) -> None:
+        """Сеттер для атрибута year."""
         if not isinstance(value, int):
             raise ValueError("Год издания должен быть целым числом.")
         current_year = date.today().year
@@ -105,10 +120,12 @@ class Book:
 
     @property
     def status(self) -> str:
+        """Геттер для атрибута status."""
         return self.__status
 
     @status.setter
     def status(self, value: str) -> None:
+        """Сеттер для атрибута status."""
         if value not in {s.value for s in Status}:
             raise ValueError(
                 "Статус книги должен быть одним из: "
@@ -117,7 +134,8 @@ class Book:
         self.__status = value.strip().lower()
 
     @staticmethod
-    def validate_non_empty_string(field_name, value):
+    def validate_non_empty_string(field_name: str, value: str) -> str:
+        """Валидатор проверяет, что атрибут является непустой строкой."""
         if not isinstance(value, str) or value.strip() == "":
             raise ValueError(
                 f"Поле {field_name} должно быть непустой строкой."
