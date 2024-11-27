6f4902dd03d5fea5e33eb41db2a8c510911e1816
bissaliev <bissaliev21@gmail.com>
2024-11-27 07:48:47 +0400

    Добавил файлового менеджера для работы с файлом CSV

--- filemanagers.py
+++ filemanagers.py
@@ -1,3 +1,4 @@
+import csv
 import json
 from abc import ABC, abstractmethod
 from pathlib import Path
@@ -5,6 +6,11 @@ from typing import Any
 
 
 class FileManager(ABC):
+    """Абстрактный класс файловый менеджер."""
+
+    def __init__(self, filename: str):
+        self.filepath = Path(__file__).parent / filename
+
     @abstractmethod
     def load(self) -> dict[str, Any]:
         pass
@@ -15,10 +21,10 @@ class FileManager(ABC):
 
 
 class JsonFileManager(FileManager):
-    def __init__(self, filename: str):
-        self.filepath = Path(__file__).parent / filename
+    """Файловый менеджер для работы с файлом JSON."""
 
     def load(self) -> list[dict[str, str | int]]:
+        """Загрузка данных из файла."""
         try:
             with self.filepath.open(encoding="utf-8") as file:
                 first_char = file.read(1)
@@ -35,6 +41,45 @@ class JsonFileManager(FileManager):
         return data
 
     def save(self, data: list[dict[str, str | int]]) -> None:
+        """Сохранение данных в файл."""
         self.filepath.parent.mkdir(parents=True, exist_ok=True)
         with self.filepath.open("w", encoding="utf-8") as file:
             json.dump(data, file, ensure_ascii=False, indent=4)
+
+
+class CSVFileManager(FileManager):
+    """Файловый менеджер для работы с файлом CSV."""
+
+    def __init__(self, filename: str, delimiter: str = ","):
+        super().__init__(filename)
+        self.delimiter = delimiter
+
+    def load(self) -> list[dict[str, str | int]]:
+        """Загрузка данных из файла."""
+        if not self.filepath.exists():
+            raise FileNotFoundError(f"Файл {self.filepath} не найден.")
+        data = []
+        with self.filepath.open(encoding="utf-8") as file:
+            try:
+                csv_reader = csv.DictReader(file, delimiter=self.delimiter)
+                for row in csv_reader:
+                    for key, value in row.items():
+                        if value.isdigit() and key in ("id", "year"):
+                            row[key] = int(value)
+                    data.append(row)
+            except csv.Error as e:
+                raise ValueError(f"Ошибка чтения CSV файла: {e}") from e
+        return data
+
+    def save(self, data: list[dict[str, str | int]]) -> None:
+        """Сохранение данных в файл."""
+        fieldnames = list(data[0].keys())
+        with self.filepath.open("w", encoding="utf-8") as file:
+            csv_writer = csv.DictWriter(
+                file, fieldnames=fieldnames, delimiter=self.delimiter
+            )
+            try:
+                csv_writer.writeheader()
+                csv_writer.writerows(data)
+            except csv.Error as e:
+                raise ValueError(f"Ошибка чтения CSV файла: {e}") from e
